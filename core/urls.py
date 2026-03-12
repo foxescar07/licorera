f11c46d199cbad95a843f01a6cc142302e41c2c1
foxescar <cscsferney@gmail.com>
2026-03-12 16:32:57 -0500

    ojo

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('licorera/', include('licorera.urls')),
-    path('proveedores/', include('proveedores.urls')),
 ]
