f45bf79acfea38acfeff4ac08c3528e8eb0b34f3
vanessaza06 <yurivanesaaza@gmail.com>
2026-03-13 16:01:20 -0500

    fix: eliminar archivos

--- proveedores/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Proveedor
-
-def proveedores(request):
-    proveedores = Proveedor.objects.all()
-    return render(request, 'proveedores/proveedor.html', {
-        'proveedores': proveedores
-    })
