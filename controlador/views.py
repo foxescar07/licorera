10c7c1662e0cc9f7e498c87f6c55823e5b12ad3b
foxescar <ccanariasogamoso@gmail.com>
2026-03-14 12:47:18 -0500

    feat: nueva ruta en el controlador para producto

--- controlador/views.py
+++ controlador/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def proveedores(request):
     return render(request, 'proveedor.html')
+
+def producto(request):
+    return render(request, 'producto.html')
