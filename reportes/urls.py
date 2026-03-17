74dcf2fc9ce83dd5c4001faedb3a5e2d981f84f7
ayalamafe35-prog <ayalamafe35@gmail.com>
2026-03-17 15:37:37 -0500

    hay te mando los cambios

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render
 
-urlpatterns = [
-    path('', views.reporte, name='reportes'),
-]
+# El nombre DEBE ser 'reporte' para que coincida con views.reporte
+def reporte(request):
+    return render(request, 'reportes.html')
--- reportes/views.py
+++ reportes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+
+# El nombre DEBE ser 'reporte' para que coincida con views.reporte
 def reporte(request):
     return render(request, 'reportes.html')
