a2ba89da88f053a86f490322d1fc97923692b12d
ayalamafe35-prog <ayalamafe35@gmail.com>
2026-03-17 12:35:44 -0500

    el cambio

--- licorera/urls.py
+++ licorera/urls.py
@@ -1,9 +1,7 @@
 
-from django.contrib import admin
 from django.urls import path
 from reportes import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('reportes/', views.reportes, name='reportes'),
+    path('', views.reporte, name='home'),
 ]
