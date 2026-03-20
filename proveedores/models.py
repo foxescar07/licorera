8de0acf4a206fe2b8ab870113ccfe13d7359b7c7
vanessaza06 <yurivanesaaza@gmail.com>
2026-03-20 17:23:11 -0500

    fixe:agrege la fecha en la que se registro

--- proveedores/models.py
+++ proveedores/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User # Este es el modelo estándar
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20)
     empresa = models.CharField(max_length=100)
-    correo = models.EmailField()
+    correo = models.EmailField(unique=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    # Aquí usamos 'User'
+    registrado_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
--- proveedores/views.py
+++ proveedores/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render, redirect
 from .forms import ProveedorForm
 from .models import Proveedor
+# IMPORTANTE: Cambiamos 'usuario.models' por el oficial de Django
+from django.contrib.auth.models import User 
 
 def inicio_proveedores(request):
     if request.method == 'POST':
         form = ProveedorForm(request.POST)
         if form.is_valid():
-            form.save() # Aquí se guarda el proveedor
-            return redirect('proveedores') # Esto refresca la página
+            try:
+                aux = User.objects.first()  
+            except User.DoesNotExist:
+                aux = None
+
+            proveedor = form.save(commit=False)
+            proveedor.registrado_por = aux
+            proveedor.save()
+
+            return redirect('proveedores')
     else:
-        form = ProveedorForm() 
-    
+        form = ProveedorForm()
+
     proveedores = Proveedor.objects.all()
-    
-    context={
+
+    context = {
         'form': form,
         'proveedores': proveedores
     }
