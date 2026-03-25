783ddad8cd06777e1cb239797417eab65e0b0995
mariana2007fonseca-pixel <mariana2007fonseca@gmail.com>
2026-03-25 13:53:33 -0500

    2

--- licorera/urls.py
+++ licorera/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.login_proyecto, name='login_url'), # El nombre debe ser igual a la función de arriba
+    path('', views.login_proyecto, name='login_url'), # Solo esta ruta
 ]
--- licorera/views.py
+++ licorera/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 def login_proyecto(request):
-    # Esto simplemente abre tu página de login
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario_input', '').strip().lower()
+        clave = request.POST.get('clave_input', '').strip()
+
+        if usuario == "LUNA" and clave == "555":
+            return render(request, 'login.html', {'mensaje': '¡Bienvenida Luna! Ingreso exitoso.'})
+        else:
+            return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
+
     return render(request, 'login.html')
