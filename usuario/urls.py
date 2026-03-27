d51da53f734074e1a0aade4ec58e3ed256a5a1ed
mariana2007fonseca-pixel <mariana2007fonseca@gmail.com>
2026-03-27 13:41:59 -0500

    .

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from . import views
- 
-urlpatterns = [
-    path('usuario/registro/', views.registro_usuario, name='registro_usuario'),
-    path('usuario/', views.lista_usuarios, name='lista_usuarios'),
-]
 
+from django.urls import path
+from . import views # Asegúrate de tener funciones en views.py
+
+urlpatterns = [
+    
+    path('', views.usuario, name='usuario'),
+]
