f6c4c3e76024263ed2fe16e9786d7b496852d218
foxescar <ccanariasogamoso@gmail.com>
2026-03-27 13:50:15 -0500

    fix: eliminacion para cambio de comflictos

--- proveedores/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio_proveedores, name='proveedores'),  
-    path('editar-proveedor/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
-    path('eliminar-proveedor/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor')
-]
