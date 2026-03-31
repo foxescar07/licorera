247d63a517670a3a04917dfdfcbec3b535d6f0ae
Andreina <daiverlynmejias3@gmail.com>
2026-03-31 16:05:38 -0500

    fix: eliminación de conteo y de otra linea

--- inventario/urls.py
+++ inventario/urls.py
@@ -5,8 +5,7 @@ app_name = 'inventario'
 
 urlpatterns = [
     path('', views.inventario_home, name='inventario_home'),
-    path('conteo/', views.conteo_inventario, name='conteo_inventario'),
-    path('comparar/', views.comparar_inventario, name='comparar_inventario'),
     path('agenda/<int:pk>/estado/', views.agenda_cambiar_estado, name='agenda_estado'),
     path('conteo/guardar/', views.guardar_conteo, name='guardar_conteo'),
+    path('conteo/', views.conteo_inventario, name='conteo_inventario'),
 ]
