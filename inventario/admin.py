61a7f8d8f2bb903764cff64f8292b580c0c34e6b
Andreina <daiverlynmejias3@gmail.com>
2026-05-07 13:41:06 -0500

    fix: agregar  apps que faltaban

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import SesionConteo, ConteoProducto
+from .models import SesionConteo, ConteoProducto, ResultadoInventario, Lote
 
-admin.site.register(SesionConteo)
-admin.site.register(ConteoProducto)
+@admin.register(SesionConteo)
+class SesionConteoAdmin(admin.ModelAdmin):
+    list_display  = ['pk', 'fecha_inicio', 'estado', 'activa', 'responsable']
+    list_filter   = ['estado', 'activa']
+
+@admin.register(ConteoProducto)
+class ConteoProductoAdmin(admin.ModelAdmin):
+    list_display  = ['sesion', 'producto', 'cantidad_contada']
+    search_fields = ['producto__nombre']
+
+@admin.register(ResultadoInventario)
+class ResultadoInventarioAdmin(admin.ModelAdmin):
+    list_display = ['sesion', 'producto', 'cantidad_sistema', 'cantidad_fisica', 'diferencia']
+
+@admin.register(Lote)
+class LoteAdmin(admin.ModelAdmin):
+    list_display  = ['numero_lote', 'producto', 'fecha_vencimiento', 'esta_vencido']
+    search_fields = ['numero_lote', 'producto__nombre']
